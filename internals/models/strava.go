aad2c38c643d504e5c728fa38e5a0084443cf22a
nullator <diver.rus@gmail.com>
2023-09-19 21:49:52 +0400

    model description update

--- internals/models/strava.go
+++ internals/models/strava.go
@@ -16,26 +16,26 @@ type AuthHandler struct {
 }
 
 type Athlete struct {
-	Id             int64    `json:"id"`             // 2534833,
-	Username       string   `json:"username"`       // "nullator_n",
-	Resource_state int      `json:"resource_state"` // 2,
-	Firstname      string   `json:"firstname"`      // "Vyacheslav",
-	Lastname       string   `json:"lastname"`       // "O",
-	Bio            string   `json:"bio"`            // "",
-	City           string   `json:"city"`           // "Samara",
-	State          string   `json:"state"`          // "Samarskaya oblast",
-	Country        string   `json:"country"`        // "Russia",
-	Sex            string   `json:"sex"`            // "M",
-	Premium        bool     `json:"premium"`        // false,
-	Summit         bool     `json:"summit"`         // false,
-	Created_at     string   `json:"created_at"`     // "2013-07-11T14:30:18Z",
-	Updated_at     string   `json:"updated_at"`     // "2021-09-10T16:14:11Z",
-	Badge_type_id  int      `json:"badge_type_id"`  // 0,
-	Weight         float32  `json:"weight"`         // 62.0,
-	Profile_medium string   `json:"profile_medium"` // "https://dgalywyr863hv.cloudfront.net/pictures/athletes/2534833/6923497/2/medium.jpg",
-	Profile        string   `json:"profile"`        // "https://dgalywyr863hv.cloudfront.net/pictures/athletes/2534833/6923497/2/large.jpg",
-	Friend         []string `json:"friend"`         // null,
-	Follower       []string `json:"follower"`       // null
+	Id             int64    `json:"id"`             // id в страве
+	Username       string   `json:"username"`       // ник
+	Resource_state int      `json:"resource_state"` //
+	Firstname      string   `json:"firstname"`      //
+	Lastname       string   `json:"lastname"`       //
+	Bio            string   `json:"bio"`            //
+	City           string   `json:"city"`           //
+	State          string   `json:"state"`          //
+	Country        string   `json:"country"`        //
+	Sex            string   `json:"sex"`            //
+	Premium        bool     `json:"premium"`        //
+	Summit         bool     `json:"summit"`         //
+	Created_at     string   `json:"created_at"`     //
+	Updated_at     string   `json:"updated_at"`     //
+	Badge_type_id  int      `json:"badge_type_id"`  //
+	Weight         float32  `json:"weight"`         //
+	Profile_medium string   `json:"profile_medium"` // ссылка на фото профиля
+	Profile        string   `json:"profile"`        // ссылка на фото профиля
+	Friend         []string `json:"friend"`         //
+	Follower       []string `json:"follower"`       //
 }
 
 type RespondRefreshToken struct {
